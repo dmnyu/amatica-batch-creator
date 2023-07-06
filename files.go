659dd8f2af14ebf0124cb3390fc57b83e8c59b03
Donald Mennerich <don.mennerich@nyu.edu>
2023-07-06 19:32:20 -0400

    adding copy functions

--- files.go
+++ files.go
@@ -2,9 +2,22 @@ package amatica_batch_creator
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
+
+const permsMode = 0777
+
+var TargetBatchDirs = []string{}
+var SourceDirectory string
+var Sourcefiles = []string{}
+var SourceFilesSlices [][]string
+
+func SetSourceDirectory(source string) {
+	SourceDirectory = source
+}
 
 func DirectoryExists(source string) error {
 	if _, err := os.Stat(source); err == nil {
@@ -21,6 +34,7 @@ func GetFileSizeAndCount(source string) (int64, int, error) {
 	var filesCount int = 0
 	err := filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
+			Sourcefiles = append(Sourcefiles, info.Name())
 			filesSize += info.Size()
 			filesCount += 1
 		}
@@ -32,4 +46,110 @@ func GetFileSizeAndCount(source string) (int64, int, error) {
 	}
 
 	return filesSize, filesCount, nil
+}
+
+func CreateTargetDirectory(targetDirectory string) error {
+	if err := os.Mkdir(targetDirectory, permsMode); err != nil {
+		return err
+	}
+	return nil
+}
+func GetNumBatchDirectories(sourceFilesCount int, maxFiles int) int {
+	numBatchDirs := sourceFilesCount / maxFiles
+	if maxFiles*numBatchDirs < sourceFilesCount {
+		numBatchDirs += 1
+	}
+	return numBatchDirs
+}
+
+func CreateBatchDirectories(targetDirectory string, baseName string, numBatchDirs int) error {
+	for i := 0; i < numBatchDirs; i++ {
+		batchDir := filepath.Join(targetDirectory, fmt.Sprintf("%s-batch-%d", baseName, i+1))
+		if err := os.Mkdir(batchDir, permsMode); err != nil {
+			return err
+		}
+		TargetBatchDirs = append(TargetBatchDirs, batchDir)
+	}
+	return nil
+}
+
+func SplitSourceFileSlice() {
+	SourceFilesSlices = [][]string{}
+
+	chunkSize := (len(Sourcefiles) + len(TargetBatchDirs) - 1) / len(TargetBatchDirs)
+
+	for i := 0; i < len(Sourcefiles); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(Sourcefiles) {
+			end = len(Sourcefiles)
+		}
+
+		SourceFilesSlices = append(SourceFilesSlices, Sourcefiles[i:end])
+	}
+}
+
+func CopyFiles() {
+	for i, sourceFileSlice := range SourceFilesSlices {
+		fmt.Println("copying files in batch", i+1)
+		for _, sourceFile := range sourceFileSlice {
+			sourcePath := filepath.Join(SourceDirectory, sourceFile)
+			targetPath := filepath.Join(TargetBatchDirs[i], sourceFile)
+			fmt.Println(sourcePath + " -> " + targetPath)
+			copyFile(sourcePath, targetPath)
+		}
+	}
+}
+
+func copyFile(src string, dst string) error {
+	sfi, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !sfi.Mode().IsRegular() {
+		// cannot copy non-regular files (e.g., directories,
+		// symlinks, devices, etc.)
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+	}
+	dfi, err := os.Stat(dst)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			return err
+		}
+	}
+
+	if err := copyFileContents(src, dst); err != nil {
+		fmt.Println("error copying file", err.Error())
+	}
+	return nil
+}
+
+func copyFileContents(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+	err = out.Sync()
+	return err
 }
--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	amatica_batch_creator "amatica-batch-creator"
 	"flag"
 	"fmt"
-	"os"
+	"path/filepath"
 )
 
 var (
@@ -24,6 +24,7 @@ func init() {
 }
 
 func main() {
+	fmt.Println("amatica-batch-creator")
 	flag.Parse()
 
 	if maxSize == 0 && maxFiles == 0 {
@@ -33,19 +34,41 @@ func main() {
 	if err := amatica_batch_creator.DirectoryExists(sourceDirectory); err != nil {
 		panic(err)
 	}
+	amatica_batch_creator.SetSourceDirectory(sourceDirectory)
 
 	if err := amatica_batch_creator.DirectoryExists(targetDirectory); err != nil {
-		if err := os.Mkdir(targetDirectory, 0644); err != nil {
+		if err := amatica_batch_creator.CreateTargetDirectory(targetDirectory); err != nil {
 			panic(err)
 		}
 	}
 
 	//get the number of files and total size of the source directory
+	fmt.Println("Counting files and calculating total size of source directory...")
 	var err error
 	sourceFilesSize, sourceFilesCount, err = amatica_batch_creator.GetFileSizeAndCount(sourceDirectory)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Total size of source directory: %d\n", sourceFilesSize)
+	fmt.Printf("Total Count of files in source directory: %d\n", sourceFilesCount)
 
-	fmt.Println(sourceFilesSize, sourceFilesCount)
+	//calculate the number of batch directories
+	fmt.Println("Calculating number of batch directories...")
+	numBatchDirs := amatica_batch_creator.GetNumBatchDirectories(sourceFilesCount, maxFiles)
+	fmt.Printf("Number of batch directories: %d\n", numBatchDirs)
+
+	//create the batch directories
+	fmt.Println("Creating batch directories...")
+	if err := amatica_batch_creator.CreateBatchDirectories(targetDirectory, filepath.Base(sourceDirectory), numBatchDirs); err != nil {
+		panic(err)
+	}
+
+	//split the sourcefiles slice into subslices
+	amatica_batch_creator.SplitSourceFileSlice()
+	for i, s := range amatica_batch_creator.SourceFilesSlices {
+		fmt.Println(i, len(s))
+	}
+
+	//copy the files
+	amatica_batch_creator.CopyFiles()
 }
